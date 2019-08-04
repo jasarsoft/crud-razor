86c260fb19e23778819c566706c3c0a60827916c
Edin Jašarević <edinjasar14@gmail.com>
2019-08-04 14:22:07 +0200

    add validation and more properties

--- Razor Example/Migrations/20190804113642_AddMorePropertiesToBook.Designer.cs
+++ Razor Example/Migrations/20190804113642_AddMorePropertiesToBook.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Razor.Example.Model;
 
 namespace Jasarsoft.Razor.Example.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190804113642_AddMorePropertiesToBook")]
+    partial class AddMorePropertiesToBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Razor Example/Model/Book.cs
+++ Razor Example/Model/Book.cs
@@ -12,5 +12,9 @@ namespace Jasarsoft.Razor.Example.Model
 
         [Required]
         public string Name { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
     }
 }
