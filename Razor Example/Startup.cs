51c4965ba4f585b24be7aafb611c511d140e038a
Edin Jašarević <edinjasar14@gmail.com>
2019-08-04 05:58:01 +0200

    connection string

--- Razor Example/Startup.cs
+++ Razor Example/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Razor.Example.Model;
 
 namespace Razor_Example
 {
@@ -31,6 +33,7 @@ namespace Razor_Example
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
